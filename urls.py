840437b58b76f7aa650ea7334a50042d50cb929a
Devligue <krzysztof.dziadowiec@gmail.com>
2015-06-21 21:41:31 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = [
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'PROJECTNAME.APPNAME.views.home', name='home')
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'app1.views.home', name='home')
 ]
