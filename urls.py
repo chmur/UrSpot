b9bf144ba9fd67d63f7ca7af9525904455ba30c5
Devligue <krzysztof.dziadowiec@gmail.com>
2015-06-21 22:56:19 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'app1.views.home', name='home')
+    url(r'^$', 'app1.views.home', name='home'),
+    url(r'^$admin/doc/', include('django.contrib.admindocs.urls')),
 ]
