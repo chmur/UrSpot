ac920ff484846b0c99fe5dcbfe3c9d8177e4e1da
Devligue <krzysztof.dziadowiec@gmail.com>
2015-06-23 04:58:18 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
+#from app1.views import home1
 
 urlpatterns = [
+    
+    url(r'^$', 'app1.views.spot_list', name='spot_list'),
+    url(r'^spot/(?P<pk>[0-9]+)/$', 'app1.views.spot_detail'),
+    url(r'^spot/new/$', 'app1.views.spot_new', name='spot_new'),
+    #url(r'', include('app1.urls'))
+    #url(r'^$admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^app1/', include('app1.urls')),
+    #url(r'^$', home1.as_view(), name='home1'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'app1.views.home', name='home'),
-    url(r'^$admin/doc/', include('django.contrib.admindocs.urls')),
 ]
