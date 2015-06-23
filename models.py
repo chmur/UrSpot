1e63dc125d0f9eaf4b00c99bd0e5a3adc9b591b8
Devligue <krzysztof.dziadowiec@gmail.com>
2015-06-23 04:59:26 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from taggit.managers import TaggableManager
 
 class spots(models.Model):
@@ -8,8 +9,16 @@ class spots(models.Model):
 	link = models.URLField()
 	description = models.TextField()
 	#rating = models.DecimalField(max_digits = 1, decimal_places = 1) # max_value = 5.0, min_value = 1.0)
-	timestamp = models.DateTimeField() 
+	timestamp = models.DateTimeField(default=timezone.now) 
 	tags = TaggableManager()
+#	published_date = models.DateTimeField(blank=True, null=True)
 
-	#def __unicode__(self):
-	#	return self.title
+#def publish(self):
+#    self.published_date = timezone.now()
+#    self.save()
+
+#def __str__(self):
+#    return self.title
+
+def __unicode__(self):
+	return self.spotname
